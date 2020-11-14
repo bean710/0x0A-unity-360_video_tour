e7a4212a2349438dfc8e46436b49b1b57376d575
Ben Keener <keener4christ@gmail.com>
2020-11-14 15:11:20 -0500

    Added other room spheres with a switcher

---
+++ Assets/Scripts/SphereSwitcher.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SphereSwitchEvent : UnityEvent<string>
+{
+
+}
+
+public static class SphereSwitcher
+{
+    private static SphereSwitchEvent MSSE = new SphereSwitchEvent();
+
+    public static void AddSphereHandler(UnityAction<string> f)
+    {
+        MSSE.AddListener(f);
+    }
+
+    public static void SwitchSphere(string sphereName)
+    {
+        if (MSSE != null)
+            MSSE.Invoke(sphereName);
+    }
+}
--- Assets/Scripts/VideoSwitch.cs
+++ Assets/Scripts/VideoSwitch.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoSwitch : MonoBehaviour
 {
+    public bool startsOn = false;
+    
+    private VideoPlayer vidComp;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidComp = GetComponent<VideoPlayer>();
+
+        vidComp.enabled = startsOn;
+
+        SphereSwitcher.AddSphereHandler(SphereChange);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SphereChange(string sphereName)
     {
         
+        vidComp.enabled = (sphereName == name);
     }
 }
