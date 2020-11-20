4eaf512632c8cad43fbbbd50fc501c8c84777705
Ben Keener <keener4christ@gmail.com>
2020-11-19 19:38:46 -0500

    Added info node to the living room

---
+++ Assets/Scripts/InfoClick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoClick : PointerClick
+{
+    public float scaleBy = 0.2f;
+
+    private Vector3 scale;
+    private bool contentIsActive = false;
+
+    public Transform icon;
+    public GameObject Content;
+
+    private void Start() {
+        scale = new Vector3(scaleBy, scaleBy, scaleBy);
+    }
+
+    public override void Click()
+    {
+        contentIsActive = !contentIsActive;
+        Content.SetActive(contentIsActive);
+    }
+
+    public override void Hover()
+    {
+        Debug.Log("Hovered");
+
+        if (icon == null)
+        {
+            Debug.Log("Icon is null");
+            return;
+        }
+
+        icon.localScale += scale;
+        //transform.localScale = new Vector3(2f, 2f, 2f);
+    }
+
+    public override void UnHover()
+    {
+        Debug.Log("Un-Hovered");
+
+        if (icon == null)
+        {
+            Debug.Log("Icon is null");
+            return;
+        }
+
+        icon.localScale -= scale;
+        //transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+}
